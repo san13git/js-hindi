7d797ea2162b058f8e472900683f635ddb5586b3
san13git <122282397+san13git@users.noreply.github.com>
2024-06-07 13:39:02 +0000

    datatypes

--- jsbasics/test.js
+++ jsbasics/test.js
@@ -1,1 +1,12 @@
-console.log("sanya")
+
+const accountId=14
+let email="sansi@23"
+let password="123"
+city="jaipur" //not preferable but possible
+let accountState; //if we print its value it will be undefined
+
+//var is not used now due to scope problem
+
+//console.table([accountId,email]) -> using console.table we can print all variables together
+console.table([accountId,email,password])
+//ecma -> wrote javascript standards
