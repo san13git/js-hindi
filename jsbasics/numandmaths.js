af620558c8636bc624ab5d59358432a77db142f7
san13git <122282397+san13git@users.noreply.github.com>
2024-06-16 14:56:20 +0000

    ch

---
+++ jsbasics/numandmaths.js
@@ -0,0 +1,6 @@
+//agar number hii chahiye toh
+const balance = new Number(100)
+console.log(balance.toString()).length()
+//toFixed -> to make precesion value eg ->balance.toFixed(2) means 2 decimal tk bs
+//toPrecision -> decimal se pehle dekhta h kitni digits mai precesion krega eg -> 123.896 aur precision mai 3 kre toh 124 hoga
+//toLocaleString -> commas dalte h , used for big numbers
