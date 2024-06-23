6d31c005c05ac34f609bf852975183ad063dcfa0
san13git <122282397+san13git@users.noreply.github.com>
2024-06-23 08:49:05 +0000

    fun

---
+++ jsbasics/functions.js
@@ -0,0 +1,7 @@
+function myfunc(num1,num2){ //parameters
+    return num1+num2;
+    //agar yha console.log() mai likhe toh aur return na likhe toh return undefined hoga
+
+}
+console.log(myfunc(3,4)); //arguments
+//if i write console.log(loginuser())  -> & i dont pass anything then it will be treated as undefined
