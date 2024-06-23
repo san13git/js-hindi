f2e25bd266749be1b721a27466b4d72ac7c0c062
san13git <122282397+san13git@users.noreply.github.com>
2024-06-23 08:08:43 +0000

    cho

--- jsbasics/objects.js
+++ jsbasics/objects.js
@@ -2,7 +2,7 @@
 //object.create
 //object can be declared by literal & constructor
 const mysm=Symbol("key")
-const user={
+const user={   //non singleton object
     "name":"sanya",
     "age":"20",
     "full name":"sanya bhasin" ,//ab sirf square bracket se hii access hoga
@@ -21,5 +21,17 @@ user.greeting=function(){
 // console.log(user.greeting()) ->undefined
 //to reference same object use this
 
-
+const user=new Object()  //non singleton object
+user.id="12" //add more
+//we can declare object inside object too
+//to concatinate 2 objects
+const obj1={1:'a',2:'b'}
+const obj2={3:'a',4:'b'}
+const obj3=Object.assign({},obj1,obj2)  //if we use {} then it acts as target rest are the source
+//... -> spread operator
+const obj3={...obj1,...obj2}
+//ek array bnakr usme bht saare objects bhi bna skte
+Object.keys(obj1) //it will give all keys of the object
+Object.values(obj1)  //it will give all the values corresponding to the key
+//if we want to check a particular propert h ya ni then use hasOwnProperty
 
