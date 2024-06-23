ff44b80d2230fed221a58ca4fd869cbf54a27adb
san13git <122282397+san13git@users.noreply.github.com>
2024-06-23 12:15:13 +0000

    hi

--- jsbasics/functions.js
+++ jsbasics/functions.js
@@ -5,3 +5,16 @@ function myfunc(num1,num2){ //parameters
 }
 console.log(myfunc(3,4)); //arguments
 //if i write console.log(loginuser())  -> & i dont pass anything then it will be treated as undefined
+
+//rest operator & spread operator -> ...
+function calPrice(...num1)
+{
+    return num1
+}
+console.log(calPrice(200,400,500,600))  //o/p -> [200,400,500,600]
+
+//**************Scopes************* */
+//{} ->tells the scope
+//var is avoided due to scope issue
+//suppose inside a block scope var is declared & i am trying to print it outside the block then it will be printed , so var is avoided & let is used
+//block scope & global scope
